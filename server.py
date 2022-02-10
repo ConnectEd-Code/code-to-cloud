86427649fd4fac2ce91ff5f527ff8ba5ac83755a
Renee Noble <renee.noble@hotmail.com>
2022-02-10 15:53:00 +1100

    no profile

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, render_template
 
-invite_app = Flask(__name__)
+app = Flask(__name__)
 
 
-@invite_app.route("/")
+@app.route("/")
 def form():
     return render_template("form.html")
 
 
-@invite_app.route("/view")
+@app.route("/view")
 def view_invite():
     # to = "Tom"
     # event = "Artemis' birthday party"
@@ -37,4 +37,4 @@ def view_invite():
 
 if __name__ == '__main__':
     # app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8000)
-    invite_app.run(debug=True, use_reloader=True)
+    app.run(debug=True, use_reloader=True)
