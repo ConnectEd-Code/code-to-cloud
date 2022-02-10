9f1c83b414fe14510bb42787a9cec8f1bce3a611
Renee Noble <renee.noble@hotmail.com>
2022-02-10 14:29:52 +1100

    appify

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, render_template
 
-invite_app = Flask('app')
+app = Flask(__name__)
 
 
-@invite_app.route("/")
+@app.route("/")
 def form():
     return render_template("form.html")
 
 
-@invite_app.route("/view")
+@app.route("/view")
 def view_invite():
     # to = "Tom"
     # event = "Artemis' birthday party"
@@ -35,7 +35,6 @@ def view_invite():
     return render_template(template, to=to, event_name=event, date=date, time=time, sender=sender)
 
 
-
 if __name__ == '__main__':
-    # invite_app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8000)
-    invite_app.run(debug=True, use_reloader=True)
+    # app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, use_reloader=True)
