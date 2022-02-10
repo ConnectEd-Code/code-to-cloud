61324bfd9ec4d5fda0c209f5c43214cf375f8a3f
Renee Noble <renee.noble@hotmail.com>
2022-02-10 14:17:51 +1100

    port 8000

--- server.py
+++ server.py
@@ -37,4 +37,4 @@ def view_invite():
 
 
 if __name__ == '__main__':
-    invite_app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8082)
+    invite_app.run(debug=True, use_reloader=True, host='0.0.0.0', port=8000)
